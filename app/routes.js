7457a8df5670f29f1ed79969b4565d71f2133d70
Colin Oakley <colin.oakley@engineering.digital.dwp.gov.uk>
2024-11-14 16:28:31 +0000

    chore: update to latest repo name

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 const journeyPluginExample = require("govuk-prototype-kit-journey-plugin");
-const alternativeFormatsPrototype = require("alternative-formats-files-prototype");
+const alternativeFormatsPrototype = require("alternative-formats-plugin");
 
 
 // Add your routes here
